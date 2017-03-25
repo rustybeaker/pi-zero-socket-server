71d158375b5d256007430f60703ac5c245f1d414
Rusty Beaker <rustybeaker@gmail.com>
2017-03-25 14:28:00 -0500

    Add files via upload

---
+++ ip.py
@@ -0,0 +1,29 @@
+import socket
+import lcd
+from threading import Timer
+# print socket.gethostbyname(socket.gethostname()) // returns 127.0.0.1
+#print socket.gethostbyname(socket.getfqdn())
+# print "working"
+
+usual_ip = "192.168.0.132"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("some-public-domain.com",80))
+cur_ip = (s.getsockname()[0])
+s.close()
+
+lcd.lcd_init()
+lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
+
+if ( usual_ip != cur_ip ):
+  lcd.lcd_string("IP "+cur_ip)
+else:
+  # print "no change"
+  #lcd.lcd_init()
+  #lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
+  lcd.lcd_string("checking ip",2)
+  
+  def print_ip():
+      lcd.lcd_string("IP "+usual_ip,2)
+  
+  Timer(1.0, print_ip).start()
